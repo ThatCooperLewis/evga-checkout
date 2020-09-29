faefe0ddf8c8bc6f3704051f3e94a758264c404f
Cooper Lewis <thatcooperlewis@gmail.com>
2020-09-28 21:29:55 -0400

    Update comments on config

--- evga_config.py
+++ evga_config.py
@@ -1,26 +1,24 @@
-#The juicy bits
+# Product Page
 login_status_true = 'pnlLoginBoxLogged'
 add_to_cart = 'LFrame_btnAddToCart'
 out_of_stock = 'LFrame_pnlNotify'
 
-#Credit card
+# Credit card
 name_on_card = 'ctl00_LFrame_txtNameOnCard'
 card_number = 'ctl00_LFrame_txtCardNumber'
 card_cvv = 'ctl00_LFrame_txtCvv'
 card_exp_month = 'ctl00_LFrame_ddlMonth'
 card_exp_year = 'ctl00_LFrame_ddlYear'
 
-#Toggles/Selects
+# Toggles/Selects
 credit_selection = 'rdoCreditCard'
 agree_to_terms = 'cbAgree'
 credit_card_verification = 'ctl00_LFrame_cbAgree'
 ship_speed_select = 'rdoShipFee'
 
-# Continue button naming scheme is iffy. Didn't really know how to clarify this easily.
-
-#The first continue button, used prior to CC info
+# The first continue button, used prior to CC info
 continue_button_initial = 'ctl00_LFrame_btncontinue'
-#The continue button that appears on the credit info.
+# The continue button that appears on the credit info.
 continue_button_card = 'ctl00_LFrame_ImageButton2'
-#The continue button that appears when ready to checkout. 
+# The continue button that appears when ready to checkout. 
 continue_button_checkout = 'btnCheckoutContinue'
